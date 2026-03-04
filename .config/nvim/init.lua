db3e011f51f7d04e61d97de5ccde1cfd2d02e303
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2026-03-04 07:59:53 +0100

    Implement VIM Plug installation in init.lua
    Add VIM Plug installation logic to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,14 @@
 local vim = vim
 local Plug = vim.fn['plug#']
 
+-- VIM Plug Install
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+  vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+  vim.o.runtimepath = vim.o.runtimepath
+  vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
 -- VIM Plug plugin list
 vim.call('plug#begin')
 
