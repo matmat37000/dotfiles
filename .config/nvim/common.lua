df9fa2dd7552477c72e1209f0501249a543d1106
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2026-03-11 09:43:25 +0100

    Update common.lua

--- .config/nvim/common.lua
+++ .config/nvim/common.lua
@@ -12,9 +12,9 @@ vim.opt.updatetime = 300
 
 -- Tab configuration
 vim.opt.scrolloff = 7
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 
