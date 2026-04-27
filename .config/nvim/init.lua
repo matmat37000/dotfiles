f3c0feb3e8d06b5f67130fb68057ade314c2dea2
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2026-04-27 14:51:54 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,6 +40,8 @@ Plug('hrsh7th/cmp-path')
 Plug('hrsh7th/cmp-cmdline')
 Plug('hrsh7th/nvim-cmp')
 
+Plug('mfussenegger/nvim-jdtls')
+
 -- Snippets
 Plug('L3MON4D3/LuaSnip', {['tag'] = 'v2.*', ['do'] = 'make install_jsregexp'})
 Plug('saadparwaiz1/cmp_luasnip')
